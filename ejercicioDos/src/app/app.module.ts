5180f103e063d69ac03c6defe5e05684c73b8b15
carodoprado <dopradocarolina@gmail.com>
2021-11-23 16:49:45 -0300

    creación componente card

--- ejercicioDos/src/app/app.module.ts
+++ ejercicioDos/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
